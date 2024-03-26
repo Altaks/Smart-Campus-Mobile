24d595dd8ab600db2090751300a6b93e9a462d3f
Altaks <altair61.dev@gmail.com>
2024-03-26 11:40:17 +0100

    feat(API): Ajout de l'entité Conseil au modèle
    Ajout de l'entité Conseil au modèle avec les attributs suivants :
    - texte : string
    - type : string
    - min? : float
    - max? : float
    
    Mise en place de la configuration API Platform pour l'entité sur la route GET(*)

---
+++ sae4api/src/Entity/Conseil.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use App\Repository\ConseilRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConseilRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection()
+    ]
+)]
+class Conseil
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 300)]
+    private ?string $texte = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $type = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $min = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $max = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): static
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMin(): ?float
+    {
+        return $this->min;
+    }
+
+    public function setMin(?float $min): static
+    {
+        $this->min = $min;
+
+        return $this;
+    }
+
+    public function getMax(): ?float
+    {
+        return $this->max;
+    }
+
+    public function setMax(?float $max): static
+    {
+        $this->max = $max;
+
+        return $this;
+    }
+}
