990689dd159347e58cb60876f6561ab8db4733a7
Altaks <altair61.dev@gmail.com>
2024-03-26 11:43:43 +0100

    feat(API): Ajout des fixtures pour l'entité Conseil

--- sae4api/src/DataFixtures/AppFixtures.php
+++ sae4api/src/DataFixtures/AppFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Conseil;
 use App\Entity\Salle;
+use App\Entity\Recommandation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -102,6 +104,67 @@ class AppFixtures extends Fixture
         $salleC005->setNom('C005');
         $salleC005->setBaseDeDonnees('sae34bdm2eq3');
         $manager->persist($salleC005);
+
+        $recommandation1 = new Recommandation();
+        $recommandation1->setTexte("Cette recommandation est extrêmement longue et continent des informations en cas de température trop élevée");
+        $recommandation1->setType("temp");
+        $recommandation1->setMax(25.0);
+        $manager->persist($recommandation1);
+
+        $recommandation2 = new Recommandation();
+        $recommandation2->setTexte("Cette recommandation est extrêmement longue et continent des informations en cas de température trop basse");
+        $recommandation2->setType("temp");
+        $recommandation2->setMin(15.6);
+        $manager->persist($recommandation2);
+
+        $recommandation3 = new Recommandation();
+        $recommandation3->setTexte("Cette recommandation est extrêmement longue et continent des informations en cas de taux d'humidité trop élevée");
+        $recommandation3->setType("hum");
+        $recommandation3->setMax(70.0);
+        $manager->persist($recommandation3);
+
+        $recommandation4 = new Recommandation();
+        $recommandation4->setTexte("Cette recommandation est extrêmement longue et continent des informations en cas de taux d'humidité trop basse");
+        $recommandation4->setType("hum");
+        $recommandation4->setMin(30.0);
+        $manager->persist($recommandation4);
+
+        $recommandation5 = new Recommandation();
+        $recommandation5->setTexte("Cette recommandation est extrêmement longue et continent des informations en cas de taux de CO2 trop élevée");
+        $recommandation5->setType("co2");
+        $recommandation5->setMax(1000.0);
+        $manager->persist($recommandation5);
+
+
+        $conseil1 = new Conseil();
+        $conseil1->setTexte("Ce conseil est extrêmement long et continent des informations en cas de température trop élevée");
+        $conseil1->setType("temp");
+        $conseil1->setMax(25.0);
+        $manager->persist($conseil1);
+
+        $conseil2 = new Conseil();
+        $conseil2->setTexte("Ce conseil est extrêmement long et continent des informations en cas de température trop basse");
+        $conseil2->setType("temp");
+        $conseil2->setMin(15.6);
+        $manager->persist($conseil2);
+
+        $conseil3 = new Conseil();
+        $conseil3->setTexte("Ce conseil est extrêmement long et continent des informations en cas de taux d'humidité trop élevée");
+        $conseil3->setType("hum");
+        $conseil3->setMax(70.0);
+        $manager->persist($conseil3);
+
+        $conseil4 = new Conseil();
+        $conseil4->setTexte("Ce conseil est extrêmement long et continent des informations en cas de taux d'humidité trop basse");
+        $conseil4->setType("hum");
+        $conseil4->setMin(30.0);
+        $manager->persist($conseil4);
+
+        $conseil5 = new Conseil();
+        $conseil5->setTexte("Ce conseil est extrêmement long et continent des informations en cas de taux de CO2 trop élevée");
+        $conseil5->setType("co2");
+        $conseil5->setMax(1000.0);
+        $manager->persist($conseil5);
 
         $manager->flush();
     }
