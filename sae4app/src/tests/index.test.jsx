593d72e6d086eb474cb2d325e84691ba2ee5c9ea
tinsuki <tinsuki.dev@gmail.com>
2024-03-28 13:30:36 +0100

    test(app): Ajout de la lib de test et ajout d'un test pour vérifier le fonctionnement de la lib
    Ajout de la lib `vitest` pour faire les test
    Ajout de la lib de coverage
    Ajout de la lib JSOM
    Ajout de deux test pour vérifier le fonctionnement de la lib

---
+++ sae4app/src/tests/index.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Base from "../Base.jsx";
+
+
+describe('Test de fonctionnement de la librairie de test', () => {
+    it('Doit retourner vrai', () => {
+        expect(true).toBe(true);
+    });
+})
+
+describe("Test du composant Base", () => {
+    it("Doit afficher le lien Accueil", () => {
+        render(<Base/>);
+        expect(screen.getByText("Accueil")).toBeInTheDocument();
+    });
+})
