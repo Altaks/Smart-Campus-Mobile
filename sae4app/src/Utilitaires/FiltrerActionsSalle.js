9fa0a5022b3f74ca4ec1f106bad57789f455e208
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-03-29 14:06:09 +0100

    feat(app): Mise à jour du filtre des actions pour prendre en compte l'heure

--- sae4app/src/Utilitaires/FiltrerActionsSalle.js
+++ sae4app/src/Utilitaires/FiltrerActionsSalle.js
@@ -1,6 +1,9 @@
 const filtrerActionsSalle = (actions, salleId) => {
+    const curTimestamp = new Date().getTime()
+    const maxTime = 5 * 60 * 1000 // 5 minutes
     return actions.filter(action => {
-        return action.salle.id === salleId
+        return action.salle.id === salleId &&
+            new Date(action.datetime).getTime() >= curTimestamp - maxTime
     })
 }
 
