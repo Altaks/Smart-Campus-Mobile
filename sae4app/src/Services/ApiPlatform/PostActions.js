ccacc78b13608af4a8c008b90f540741fd0ff749
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-03-29 14:06:40 +0100

    fix(app): Mise à jour des routes IRI qui ne fonctionnaientt pas

--- sae4app/src/Services/ApiPlatform/PostActions.js
+++ sae4app/src/Services/ApiPlatform/PostActions.js
@@ -3,9 +3,10 @@ const PostActions = (salleId, recommandationId) => {
     return new Promise((resolve, reject) => {
             fetch(apiConfig.uriBase + apiConfig.apiPath + apiConfig.actionsPath, {
                 method:'POST',
+                headers: {accept: 'application/json', 'Content-Type': 'application/json'},
                 body: JSON.stringify({
-                    recommandation: apiConfig.apiPath + apiConfig.recommandationsPath + recommandationId,
-                    salle: apiConfig.apiPath + apiConfig.sallesPath + salleId
+                    recommandation: apiConfig.apiPath + apiConfig.recommandationsPath + '/' + recommandationId,
+                    salle: apiConfig.apiPath + apiConfig.sallesPath + '/' + salleId
                 })
             })
                 .then(response => response.json())
