e6c2e1af13729128aaea4ea0039093d4749fdb70
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-03-29 14:08:57 +0100

    feat(app): Ajout du bouton ajoutant une action
    Ajout d'un bouton permettant de valider l'action, timer de 5 sec avant envoie du post (ne peut pas encore être annulé durant les 5 sec de timeout)

--- sae4app/src/Components/Accueil/Recommandations/Recommandation.jsx
+++ sae4app/src/Components/Accueil/Recommandations/Recommandation.jsx
@@ -1,9 +1,18 @@
 import PropTypes from "prop-types"
-import {AirVent, ChevronLeft, ChevronRight, Droplets, ThermometerSnowflake, ThermometerSun} from "lucide-react";
+import {
+    AirVent,
+    ChevronLeft,
+    ChevronRight,
+    Droplets,
+    SquareCheck, SquareX,
+    ThermometerSnowflake,
+    ThermometerSun
+} from "lucide-react";
+import {PostActions} from "../../../Services/ApiPlatform/PostActions.js";
 
 const acceptedTypes = ["temp", "hum", "co2"]
 
-const Recommandation = ({type, texte, min, max, unite}) => {
+const Recommandation = ({recommandationId, type, texte, min, max, unite, salleId}) => {
 
     const isValid = (type, min, max) => {
         return !(min != null && max != null) && acceptedTypes.includes(type);
@@ -64,11 +73,58 @@ const Recommandation = ({type, texte, min, max, unite}) => {
             <span className={`text-[${color()}]`}>{max}{unite}</span>
     }
 
+    const postAction = () => {
+
+        document.getElementById("post" + recommandationId).classList.remove("translate-y-[12px]")
+
+        document.getElementById("timer" + recommandationId).classList.replace("opacity-0", "opacity-100")
+        document.getElementById("cross" + recommandationId).classList.replace("opacity-0", "opacity-100")
+        document.getElementById("cross" + recommandationId).classList.add("-translate-y-12")
+        document.getElementById("check" + recommandationId).classList.replace("opacity-100", "opacity-0")
+        document.getElementById("check" + recommandationId).classList.add("-translate-y-12")
+
+        setTimeout(() => {
+            document.getElementById("timer" + recommandationId).textContent = "4"
+            setTimeout(() => {
+                document.getElementById("timer" + recommandationId).textContent = "3"
+                setTimeout(() => {
+                    document.getElementById("timer" + recommandationId).textContent = "2"
+                    setTimeout(() => {
+                        document.getElementById("timer" + recommandationId).textContent = "1"
+                        setTimeout(() => {
+                            document.getElementById("timer" + recommandationId).textContent = "0"
+                            PostActions(salleId, recommandationId).then(r => {
+                                    if(r.status === undefined) {
+                                        document.getElementById(recommandationId).classList.add("-translate-x-[100vw]")
+                                        setTimeout(() => {
+                                            document.getElementById(recommandationId).classList.remove("max-h-52",  "border-2", "mb-3", "py-4")
+                                            document.getElementById(recommandationId).classList.add("max-h-0")
+                                        },1000)
+                                    }
+                                }
+                            )
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+
+
+
+
+
+
+    }
+
+    const cancelPost = () => {
+
+    }
+
     const render = () => {
         if (!isValid(type, min, max)) return <></>
 
         return (
-            <div className={`flex py-4 flex-row border-2 rounded-lg my-3 mx-auto mt-0`}>
+            <div id={recommandationId} className={"flex flex-row items-center border-2 rounded-lg py-4 mb-3 mx-auto max-h-52 transition-all duration-1000 overflow-hidden"}>
                 <div className="text-md w-1/4 mr-2 flex flex-col justify-center">
                     <div>
                         {incone()}
@@ -78,8 +134,15 @@ const Recommandation = ({type, texte, min, max, unite}) => {
                         {probText()}
                     </div>
                 </div>
-                <div className="text-md w-3/4 ">La {stype()} est <span className={`text-[${color()}]`}>{prob()}</span>
+                <div className="text-md w-3/4 text-justify">La {stype()} est <span className={`text-[${color()}]`}>{prob()}</span>
                     <p>{texte}</p>
+                </div>
+                <div id={"post" + recommandationId} className={"flex flex-col items-center text-center mr-2 ml-5 duration-500 transition-all translate-y-[12px]"}>
+                    <div className={"max-h-12"}>
+                        <SquareCheck id={"check" + recommandationId} color={'#22c55e'} size={48} className={'opacity-100 duration-500 transition-all'} onClick={postAction}/>
+                        <SquareX id={"cross" + recommandationId} color={'#dc2626'} size={48} className={'opacity-0 duration-500 transition-all'} onClick={cancelPost}/>
+                    </div>
+                    <p id={"timer" + recommandationId} className={" transition-all duration-500 opacity-0"}>5</p>
                 </div>
             </div>
         )
