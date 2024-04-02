5b8be4a37c487d60ca05f1a561ab36f2b4a8f929
Adrien Panis <adrien.panis@etudiant.univ-lr.fr>
2024-04-02 14:47:16 +0200

    refactor(App): Ajout commentaires sur la fonction animationLogoEtTitreEtGraphique

--- sae4app/src/Utilitaires/animationLogoEtTitreEtGraphique.js
+++ sae4app/src/Utilitaires/animationLogoEtTitreEtGraphique.js
@@ -1,15 +1,31 @@
 const animationLogoEtTitreEtGraphique = () => {
-    document.getElementById("logo").classList.add("translate-x-[100vw]", "transition-all", "duration-1000")
+    const logoRender = document.getElementById("logo")
     const titreRender = document.getElementById("titre")
+    const listeGraphique = document.getElementById("listeGraphique")
+    const changeSalle = document.getElementById("ChangeSalle")
+
+
+    // Déplacement du logo vers la droite
+    logoRender.classList.add("translate-x-[100vw]", "transition-all", "duration-1000")
+
+    // Déplacement du titre vers la gauche
     titreRender.classList.add("-translate-x-[100vw]", "transition-all", "duration-1000")
 
+    // Après que les animations de déplacement du logo et du titre soient terminées
     setTimeout(() => {
-        document.getElementById("logo").classList.remove("w-80", "my-16")
-        document.getElementById("logo").classList.add("w-0", "my-0")
-        document.getElementById("ChangeSalle").classList.remove("mt-10")
-        if (titreRender.offsetHeight > 36) document.getElementById("ChangeSalle").classList.add("-mt-10") // Si le titre est sur 2 lignes
-        document.getElementById("listeGraphique").classList.remove("translate-x-[100vw]")
-        document.getElementById("listeGraphique").classList.add("transition-all", "duration-1000")
+        // Disparition du logo
+        logoRender.classList.remove("w-80", "my-16")
+        logoRender.classList.add("w-0", "my-0")
+
+        // Retrait de l'epacement au dessus du titre
+        changeSalle.classList.remove("mt-10")
+
+        // Si le titre est sur 2 lignes,  ajout d'un epacement relatif négatif au dessus du titre
+        if (titreRender.offsetHeight > 36) changeSalle.classList.add("-mt-10")
+
+        // Déplacement du composant listegraphique vers la gauche
+        listeGraphique.classList.remove("translate-x-[100vw]")
+        listeGraphique.classList.add("transition-all", "duration-1000")
     }, 650)
 }
 
